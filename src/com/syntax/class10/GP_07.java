f6d7775912ecedc47683df19bf18cab4f9a34163
habibfedaee <habib.fedaee@gmail.com>
2022-02-17 16:28:02 -0800

    second commit

--- src/com/syntax/class10/GP_07.java
+++ src/com/syntax/class10/GP_07.java
@@ -6,15 +6,17 @@ public class GP_07 {
 
         // The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
         // The next number is found by adding up the two numbers before it:.
+        int a = 0;
+        int b = 1;
+        int counter = 1;
 
-        int[] fibonacci = new int[12];
-        fibonacci[0]=0; fibonacci[1]=1;
-        for(int i=0; i<=fibonacci.length; i++){
-            fibonacci[i+2] = fibonacci[i]+fibonacci[i+1];
-            System.out.print(fibonacci[i]+" ");
-//            if (fibonacci[i+2]<45) {
-//                break;
-//            }
+        while (counter <= 10) {
+            System.out.println(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+            counter++;
         }
+
     }
 }
